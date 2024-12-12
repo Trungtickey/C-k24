92a8c113c40ff749e2ca6020269dd5ad2021f871
Trungtickey <hocongthanhtrungvn@gmail.com>
2024-12-12 10:36:22 +0700

    commit

--- SS3/bt7.c
+++ SS3/bt7.c
@@ -1,36 +1,16 @@
 #include <stdio.h>
 int main(){
-//    int thousandDigit, hundredDigit, dozenDigit, unitDigit;
-//    printf("Nhap so hang nghin:");
-//    scanf("%d", &thousandDigit);
-//    printf("Nhap so hang tram:");
-//    scanf("%d", &hundredDigit);
-//    printf("Nhap so hang chuc:");
-//    scanf("%d", &dozenDigit);
-//    printf("Nhap so hang don vi:");
-//    scanf("%d", &unitDigit);
-//    printf("Tong cac chu so cong lai %d %d %d %d\n", thousandDigit + hundredDigit + dozenDigit + unitDigit );
+    int number, thousandDigit, hundredDigit, dozenDigit, unitDigit, sum;
+    printf("Nhap so nguyen co 4 chu so: ");
+    scanf("%d", &number);
+   // Tách các chữ số:
+   thousandDigit = number / 1000;
+   hundredDigit = (number % 1000) / 100;
+   dozenDigit = (number % 100) / 10;
+   unitDigit = number % 10;
+   sum = thousandDigit + hundredDigit + dozenDigit + unitDigit;
 
-        int number, sum = 0;
-
-
-        printf("Nhap mot so nguyen co 4 chu so: ");
-        scanf("%d", &number);
-
-
-        if (number < 1000 || number > 9999) { // Không hợp lệ nếu nhập không đủ 4 chữ số
-            printf("So nhap vao khong hop le. Vui long nhap mot so co 4 chu so.\n");
-            return 1;
-        }
-
-        if ()
-//        while (number > 0) {
-//            sum += number % 10; // Lấy chữ số cuối cùng
-//            number /= 10;       // Loại bỏ chữ số cuối cùng
-//        }
-
-        // Hiển thị tổng các chữ số
-        printf("Tong cac chu so: %d\n", sum);
+   printf("Tong 4 chu so la: %d", sum);
 
         return 0;
 }
--- SS3/bt8.c
+++ SS3/bt8.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int number, reversed = 0;
-    printf("Nhap mot so nguyen co 4 chu so: ");
-    scanf("%d", &number);
-    if (number < 1000 || number > 9999) {
-        printf("So nhap vao khong hop le. Vui long nhap mot so co 4 chu so.\n");
-        return 1;
-    }
+//    int number, reversed = 0;
+//    printf("Nhap mot so nguyen co 4 chu so: ");
+//    scanf("%d", &number);
+//    if (number < 1000 || number > 9999) {
+//        printf("So nhap vao khong hop le. Vui long nhap mot so co 4 chu so.\n");
+//        return 1;
+//    }
+//
+//    // Tính số nghịch đảo
+//    while (number > 0) {
+//        reversed = reversed * 10 + number % 10;
+//    }
+//    printf("So nghich dao: %d\n", reversed);
 
-    // Tính số nghịch đảo
-    while (number > 0) {
-        reversed = reversed * 10 + number % 10;
-    }
-    printf("So nghich dao: %d\n", reversed);
+        int number, thousandDigit, hundredDigit, dozenDigit, unitDigit;
+        printf("Nhap so nguyen co 4 chu so: ");
+        scanf("%d", &number);
+        // Tách các chữ số:
+        thousandDigit = number / 1000;
+        hundredDigit = (number % 1000) / 100;
+        dozenDigit = (number % 100) / 10;
+        unitDigit = number % 10;
+        printf("So nghich dao: %d%d%d%d\n", unitDigit, dozenDigit, hundredDigit, thousandDigit);
 
     return 0;
 }
